bda2dced38c3102754d2aa206787bb69f87b30ef
ravidkrisi <ravidkrisi22@gmail.com>
2022-11-17 17:12:44 +0200

    added test.txt

---
+++ Main.java
@@ -0,0 +1,49 @@
+package Matalot_BA;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        UndoableStringBuilder maya = new UndoableStringBuilder();
+        maya.append("ravid");
+        maya.insert(20, "raf");
+
+
+
+
+
+
+
+
+//        maya.delete(3,4);
+//        maya.append("abcdefghijk");
+//        System.out.println(maya);
+//        maya.delete(4,100);
+       // System.out.println(maya);
+
+//        maya.append("to be or not to be");
+//        System.out.println(maya);
+//        maya.replace(3, 5, "eat");
+//        System.out.println(maya);
+//        maya.replace(17, 19, "eat");
+//        System.out.println(maya);
+//        maya.reverse();
+//        System.out.println(maya);
+//        System.out.println();
+//        maya.undo();
+//        System.out.println(maya);
+//        maya.undo();
+//        System.out.println(maya);
+//        maya.undo();
+//        System.out.println(maya);
+
+
+
+
+
+
+    }
+
+
+
+
+}
